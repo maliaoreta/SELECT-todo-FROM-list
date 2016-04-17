a5c29697aab28324a4e7a6d12f0828b62a6ea3c8
maliaoreta <maliaoreta@gmail.com>
2016-04-16 16:36:53 -1000

    finishes up to 8 ix

---
+++ todo_app.sql
@@ -0,0 +1,48 @@
+DROP USER IF EXISTS michael;
+
+CREATE ROLE michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+
+DROP DATABASE IF EXISTS todo_app;
+
+CREATE DATABASE todo_app;
+
+\c todo_app
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP,
+  completed BOOLEAN NOT NULL DEFAULT false
+);
+
+ALTER TABLE tasks
+  DROP completed;
+
+ALTER TABLE tasks
+  ADD completed_at TIMESTAMP;
+
+ALTER TABLE tasks
+  ALTER updated_at SET NOT NULL;
+ALTER TABLE tasks
+  ALTER updated_at SET DEFAULT now();
+
+INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise');
+
+INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation');
+
+SELECT title FROM tasks WHERE completed_at is NULL;
+
+UPDATE tasks
+  SET completed_at = now()
+  WHERE title = 'Study SQL';
+
+SELECT title, description FROM tasks WHERE completed_at is NULL;
+
+SELECT * FROM tasks ORDER BY created_at DESC;
+
+
+
+-- SELECT * FROM tasks;
+-- \d+ tasks;
